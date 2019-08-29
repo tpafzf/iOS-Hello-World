047a085ac748af88018907c0f8d6de00c87c8ec2
Travis Agne <travis@Traviss-MacBook-Pro.local>
2019-08-29 13:47:24 -0500

    First commit of new project

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Travis Agne on 8/29/19.
+//  Copyright © 2019 Travis Agne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
